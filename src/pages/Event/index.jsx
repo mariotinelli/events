68e785950ef4a7a092d7965946e1cbc85a45400c
MARIO HENRIQUE RODRIGUES TINELLI <maario_tinelli@hotmail.com>
2022-04-07 19:58:18 -0300

    bugs solved

--- src/pages/Event/index.jsx
+++ src/pages/Event/index.jsx
@@ -10,82 +10,81 @@ import useFetch from '../../useFetch';
 
 const Event = () => {
 
-  const {id} = useParams();
-  const [loading, setLoading] = useState(true);
-  const [event, setEvent] = useState({});
-  const [eventType, setEventType] = useState({});
-  const imageURL = "http://localhost:8000/storage/img/";
+    const {id} = useParams();
+    const [loading, setLoading] = useState(true);
+    const [event, setEvent] = useState({});
+    const [eventType, setEventType] = useState({});
+    const imageURL = "http://localhost:8000/storage/img/";
 
-  useEffect(() => {
-      fetch(`http://localhost:8000/api/event/${id}`, {
-          method: "GET",
-          headers: {
-            "Content-type": "application/json",
-          }
-      })
-      .then(resp => resp.json())
-      .then(data => {
-          fetch(`http://localhost:8000/api/event_type/${data.event_type_id}`, {
+    useEffect(() => {
+        fetch(`http://localhost:8000/api/event/${id}`, {
             method: "GET",
             headers: {
               "Content-type": "application/json",
             }
+        })
+        .then(resp => resp.json())
+        .then(data => {
+            fetch(`http://localhost:8000/api/event_type/${data.event_type_id}`, {
+              method: "GET",
+              headers: {
+                "Content-type": "application/json",
+              }
+            })
+            .then(resp => resp.json())
+            .then(data => {
+                setEventType(data);
+            })
+            .catch(err => console.log(err))
+              
+          setEvent(data);
+          setLoading(false);
+        })
+        .catch(err => console.log(err))
+    }, [])
+
+   
+    function confirmPresence() {
+
+      event.participants += 1;
+
+      fetch(`http://localhost:8000/api/event/update/${id}`, {
+            method: "POST",
+            headers: {
+              "Content-type": "application/json",
+            },
+            body: JSON.stringify(event),          
           })
-          .then(resp => resp.json())
-          .then(data => {
-              setEventType(data);
-          })
-          .catch(err => console.log(err))
-            
-        setEvent(data);
-        setLoading(false);
-      })
-      .catch(err => console.log(err))
-  }, [])
-
-  
-
-  if (loading) {
-    return <h1>Carregando...</h1>
-  }
-
-  function confirmPresence() {
-
-    event.participants += 1;
-
-    fetch(`http://localhost:8000/api/event/${id}`, {
-          method: "PUT",
-          headers: {
-            "Content-type": "application/json",
-          },
-          body: JSON.stringify(event),          
-      })
-      .then((resp) => resp.json())
-      .then((data) => setEvent(data))
-      .catch( (err) => console.log(err) )
-  }
- 
-  return (
-    <EventComponent>
-      <Image src={imageURL+event.img}/>
-      <DataContainer>
-        <Title>{event.title}</Title>
-        <Data>
-          <Infos>
-            <Info> <IoPersonOutline/> Owner</Info>
-            <Info> <IoCalendarOutline/> {dateFormat(event.date)} </Info>
-            <Info> <IoLocationOutline/> {event.locality} </Info>
-          </Infos>
-          <Infos>
-            <Info> <IoPeopleOutline/> {event.participants} Participantes</Info>
-            <Info> <IoGlobeOutline/> {eventType.name} </Info>
-          </Infos>
-        </Data>
-        <Button onClick={confirmPresence} variant="contained" size="medium"> Confirmar Presença </Button>
-      </DataContainer>
-      <Description>
-        {event.description}
-      </Description>
+          .then((resp) => resp.json())
+          .then((data) => setEvent(data))
+          .catch( (err) => console.log(err) )
+    }
+    
+    if (loading) {
+      return <h1>Carregando...</h1>
+    }
+      
+    return (
+      <EventComponent>
+        <Image src={imageURL+event.img}/>
+        <DataContainer>
+          <Title>{event.title}</Title>
+          <Data>
+            <Infos>
+              <Info> <IoPersonOutline/> Owner</Info>
+              <Info> <IoCalendarOutline/> {dateFormat(event.date)} </Info>
+              <Info> <IoLocationOutline/> {event.locality} </Info>
+            </Infos>
+            <Infos>
+              <Info> <IoPeopleOutline/> {event.participants} Participantes</Info>
+              <Info> <IoGlobeOutline/> {eventType.name} </Info>
+            </Infos>
+          </Data>
+          <Button onClick={confirmPresence} variant="contained" size="medium"> Confirmar Presença </Button>
+        </DataContainer>
+        <Description>
+          {event.description}
+        </Description>
     </EventComponent>
   )  
 }
